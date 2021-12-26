b539f3519b8b47de4ebad8793db686b7579ecd13
pauldestpaer <pauldestpaer@gmail.com>
2021-12-26 10:34:30 +0000

    7, + 8's input

---
+++ 7/7_2.py
@@ -0,0 +1,56 @@
+def fuel(moveTo, positions):
+    fuel = 0
+    for crab in positions:
+        distance = abs(moveTo - crab)
+        # Fuel is now a function of distance described as ((dist**2)+dist)/2
+        fuel += ((distance**2)+distance)/2
+
+    return fuel
+
+positions = []
+maxPosition = 0
+with open('7\\7.txt') as f:
+    for line in f.readlines():
+        # just one line to read here. Split it by ','...
+        for crab in line.split(','):
+            positions.append(int(crab))
+            #if int(crab) > maxPosition:
+            #    maxPosition = int(crab)
+
+# Get the average of positions, and the nearest common position
+
+test = round(max(positions)/2)
+moveBy = test
+bestFuel = fuel(test, positions)
+moveUp = 0
+moveDown = 0
+
+done = False
+while not done:
+    # Iterate through numbers by halving gaps each time to find the optimal position to go to
+    moveBy = round(moveBy/2)
+    if moveBy < 1:
+        moveBy = 1 # in case of rounding silliness
+
+    moveUp = round(test + moveBy)
+    newFuel = fuel(moveUp, positions)
+    if newFuel < bestFuel:
+        # Beaten previous best
+        bestFuel = newFuel
+        test = moveUp
+        continue
+
+    moveDown = round(test - moveBy)
+    newFuel = fuel(moveDown, positions)
+    if newFuel < bestFuel:
+        # Beaten previous best
+        bestFuel = newFuel
+        test = moveDown
+        continue
+ 
+    # Neither success condition was hit.
+    if moveBy == 1:
+        break
+
+print(test)
+print(bestFuel)
